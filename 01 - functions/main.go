a624986f73c82756077d831a3f40cdf4aab711b2
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2022-04-09 13:32:40 +0430

    feat: addded some shitty functions to cover functions in golang

--- 01 - functions/main.go
+++ 01 - functions/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type shit struct {
+	shittyProperty string
+}
+
+func main() {
+	shittyVoid()
+
+	shittyWithMultipleReturnTypes()
+
+	shittyWithOneReturnValueOfString()
+
+	shittyValue := shit{shittyProperty: "Shit"}
+
+	shittyWithAnInput(shittyValue)
+
+}
+
+// define a void function
+func shittyVoid() {
+	log.Println("Hey I'm shitty void function without any return value")
+
+	shittyString := shittyWithOneReturnValueOfString()
+
+	fmt.Println(shittyString)
+}
+
+func shittyWithOneReturnValueOfString() string {
+	log.Println("Hey I'm shitty function with a return value of type string")
+
+	return "Shitty value"
+}
+
+func shittyWithMultipleReturnTypes() (string, int, []byte) {
+	log.Println("Hey I'm shitty function multiple return type")
+	shittyString := "shitty value"
+	shittyByteSlice := []byte{uint8(1)}
+	shittyInt := 8
+
+	return shittyString, shittyInt, shittyByteSlice
+}
+
+func shittyWithAnInput(shittyInput shit) bool {
+	log.Println("Hey I'm shitty function with an input param")
+	log.Println(shittyInput)
+	return true
+}
